65a0b315c49aec9c897f95c3d106d93e9cf2ea74
Brandon Spear <Bowdowntotheepicturkey@gmail.com>
2023-04-18 19:46:25 -0700

    push to github

---
+++ rps/RPGMain.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace RockDeckWins
+{
+public class RPSCode
+{
+  static void Main()
+  {
+    Console.WriteLine("Hello World!");
+    // C# Program code goes here.
+  }
+    static void Match()
+  {
+    Console.WriteLine("Hello World! Two");
+    // C# Program code goes here.
+  }
+}
+}
